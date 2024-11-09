5ed1c310d99b8b7c2d1c3807b8116df9461028a7
Raul Figueira <raulfmiranda@gmail.com>
2024-11-09 13:48:18 +0000

    index.js update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amor, você quer ir para a palestra da Nova Acrópole no sábado?</h1>
+    return <h1>Amor, você quer ir para a palestra da Nova Acrópole no sábado? Sim?</h1>
 }
 
 export default Home;
